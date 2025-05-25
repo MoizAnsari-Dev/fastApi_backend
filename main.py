b82db3896d673b683ad32695c4036d5e9bade08c
Moiz Ansari <moizansari.india@gmail.com>
2025-05-25 16:03:46 +0530

    Loading data from json

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+from typing import Union
 from fastapi import FastAPI
-
+import json
 app = FastAPI()
+
+def load_data():
+    with open('patients.json', 'r') as file:
+        data = json.load(file)
+    return data
+        
+    
 
 @app.get('/')
 def read_root():
@@ -9,3 +17,8 @@ def read_root():
 @app.get('/items/{item_id}')
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "query": q}
+
+@app.get('/views')
+def get_patients():
+    data = load_data()
+    return data
